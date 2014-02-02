19d587a00e6953f32aee4bdd43edff60ff992dd5
Thomas Sarlandie <thomas@sarlandie.net>
2014-02-02 00:02:26 -0800

    v1.0.5 (to test the appstore upgrade)

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -24,7 +24,7 @@ Pebble.sendAppMessageWithRetry = function(message, retryCount, successCb, failed
 
 Pebble.addEventListener("ready",
   function(e) {
-    console.log("JS Starting... v1.0.4");
+    console.log("JS Starting... v1.0.5");
     doUpdate();
   }
 );
